84f88a9da4af0beb319de7552fc1d6e9ffca1c6e
Murali Mohan <muralisocial123@gmail.com>
2022-10-18 02:27:30 +0000

    Java Modified Code

--- addition.java
+++ addition.java
@@ -1,15 +1,19 @@
-class Main {
+import java.util.Scanner;
+public class AddTwoNumbers2 {
 
-  public static void main(String[] args) {
-    
-    System.out.println("Enter two numbers");
-    int first = 10;
-    int second = 20;
-    
-    System.out.println(first + " " + second);
-
-    // add two numbers
-    int sum = first + second;
-    System.out.println("The sum is: " + sum);
-  }
+    public static void main(String[] args) {
+        
+        int num1, num2, sum;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First Number: ");
+        num1 = sc.nextInt();
+        
+        System.out.println("Enter Second Number: ");
+        num2 = sc.nextInt();
+        
+        sc.close();
+        sum = num1 + num2;
+        System.out.println("Sum of these numbers: "+sum);
+    }
 }
+
