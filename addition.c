615e09464765711d42b268dbd64612e25e24a9c9
Mohan <muralk1403@gmail.com>
2022-10-18 02:09:38 +0000

    Modified file from Sydny

--- addition.c
+++ addition.c
@@ -1,10 +1,8 @@
 #include<stdio.h>
 int main()
 {
-	int a ,b;
+	int a=10 ,b=20;
 	float c;
-	printf("Enter a and b values");
-	scanf("%d%d",&a,&b);
 	c=a+b;
 	printf("Addition of two numbers is:",c);
 
